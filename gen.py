bf0783af44c858edb3e27aa43e4812ab445d249d
Mid0aria <infoihsandemir@gmail.com>
2024-02-23 20:09:49 +0300

    added ctrl+c detect and fixed minor bug

--- gen.py
+++ gen.py
@@ -24,21 +24,26 @@ class elaina:
                     headers=headers,
                 )
             except Exception as e:
-                print(f"Hata alındı: {e}")
+                print(f"Error: {e}")
 
     def ilue(self, uid):
+        global success, fail, total
         boost = elaina().e(uid)
         try:
             with open("promos.txt", "a") as f:
                 f.write(
                     f"https://discord.com/billing/partner-promotions/1180231712274387115/{boost.json()['token']}\n\n"
                 )
+                success += 1
+                total += 1
                 return True
         except Exception as e:
+            fail += 1
+            total += 1
             return False
 
 
-worker_sayisi = int(input("How many threads would you like to use?(recommended: 10): "))
+thrds = int(input("How many threads would you like to use?(recommended: 10): "))
 print(
     """
 made with love ...
@@ -51,15 +56,12 @@ success = 0
 fail = 0
 total = 0
 
-with ThreadPoolExecutor(max_workers=worker_sayisi) as executor:
+executor = ThreadPoolExecutor(max_workers=thrds)
+
+try:
     while True:
         uid = str(uuid.uuid4())
-        if executor.submit(elaina().ilue, uid).result():
-            success += 1
-            total += 1
-        else:
-            fail += 1
-            total += 1
+        executor.submit(elaina().ilue, uid)
         print(
             colorama.Fore.GREEN
             + f"\rSuccess: {success}"
@@ -74,3 +76,6 @@ with ThreadPoolExecutor(max_workers=worker_sayisi) as executor:
             + colorama.Fore.RESET,
             end="",
         )
+except KeyboardInterrupt:
+    print("\nCtrl+C detected, stopping threads...")
+    executor.shutdown(wait=False)
